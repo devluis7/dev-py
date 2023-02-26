ee055bceb71c33469b7d6a1fb99dc9495d43ffab
devluis71~ <dev-luis@outlook.com>
2023-02-26 00:21:59 -0500

    Empezando python

---
+++ python-01/monedas3.py
@@ -0,0 +1,8 @@
+#Programa para convertir monedas
+#Version 1.0 - convertir soles a dolares
+#Inputs - entradas
+montoSoles = input("Ingrese el monto en soles")
+#Proceso
+montoDolares = montoSoles * 3.89
+#Outputs - Salidas
+print ("El monto en dolares es:" + montoDolares)
