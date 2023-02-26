0d46d5360fc97928e56e51ca82928f75553f2624
devluis71~ <dev-luis@outlook.com>
2023-02-26 01:24:22 -0500

    terminando

--- python-01/monedas.py
+++ python-01/monedas.py
@@ -1,10 +1,19 @@
 #Programa para convertir monedas
 #Version 1.0 - convertir soles a dolares
 #Inputs - entradas
-montoSoles = input("Ingrese el monto en soles: ")
+montoOrigen = input("Ingrese el monto: ")
 #Proceso
-montoDolares = float(montoSoles) / 3.89
-montoDolaresFormato = "$ {:,.2f}".format(montoDolares)
-#Outputs - Salidas
-print ("El monto en dolares es: " + str(montoDolares))
-print(montoSoles," es igual a ", str(montoDolaresFormato))
+print(" Opcion 1 - soles a dolares")
+print(" Opcion 2 - dolares a soles")
+opcion = input ("Eliga una opción: ")
+
+if (opcion ==1):
+        montoDolares = float(montoOrigen) / 3.89
+        montoDolaresFormato = "$ {:,.2f}".format(montoDolares)
+        #Outputs - Salidas
+        print ("El monto en dolares es: " + str(montoDolares))
+else:
+        montoSoles = float(montoOrigen) * 3.89
+        montoSolesFormato =  "S/. {:,.2f}".format(montoSoles)
+        #Outputs - Salidas
+        print ("El monto en soles es: " + str(montoSolesFormato))
